8aca1b9c924517d8ead07bc5e70268ffbcca19f1
Ilkcan Ustoglu <iustoglu@k1m32s09.42istanbul.com.tr>
2022-10-02 17:04:02 +0300

    minitalk

---
+++ client.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iustoglu <iustoglu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/26 18:52:07 by iustoglu          #+#    #+#             */
+/*   Updated: 2022/06/29 16:46:46 by iustoglu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void	ft_kill(int pid, char *str)
+{
+	int		i;
+	char	c;
+
+	while (*str)
+	{
+		i = 8;
+		c = *str++;
+		while (i--)
+		{
+			if (c >> i & 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(50);
+		}
+	}
+	i = 8;
+	while (i--)
+	{
+		kill(pid, SIGUSR2);
+		usleep(50);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		ft_printf("Hatalı Giriş !");
+		return (0);
+	}
+	ft_kill(ft_atoi(argv[1]), argv[2]);
+}
---
+++ minitalk.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iustoglu <iustoglu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/23 12:50:00 by iustoglu          #+#    #+#             */
+/*   Updated: 2022/06/29 16:50:43 by iustoglu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <signal.h>
+# include <unistd.h>
+# include "./ft_printf/ft_printf.h"
+
+#endif
